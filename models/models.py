6d19f5335ade2d34b67fb3ef7b2dc8482b6dd43e
Mikel López <mlvillarroya@gmail.com>
2023-12-27 21:41:48 +0100

    change in model

--- models/models.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-
-class NoSaleOptions(models.Model):
-    _name = 'no_sale_options'
-    _description = 'What if the custome does not sell'
-
-    name = fields.Char()
